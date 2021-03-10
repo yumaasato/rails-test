e961939f11b06d62ec27f032208b6e305b5fdeb2
yumaasato <asson0730@icloud.com>
2021-03-10 11:14:14 +0900

    @boardsで掲示板の内容を表示できるようにした

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,20 @@
 class BoardsController < ApplicationController
   def index
+    @boards = Board.all #boardモデルを使ってデータを取得する
   end
 
   def new
     @board = Board.new
+  end
+
+  def create
+    Board.create(board_params) #createメソッドの引数に保存するパラメータを渡すことで保存できる
+    binding.pry
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'boards', to: 'boards#index' # get “boards”→ getメソッドで/boardsにリクエストが来た場合, to: ”boards#index”→ BoardsControllerメソッド（アクション）が呼ばれるように定義している。
-  
+
   get 'boards/new', to: 'boards#new'
+  post 'boards', to: 'boards#create'
 end
