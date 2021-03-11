d95e43e99a3927c9b462227da5552ffa658a103c
yumaasato <asson0730@icloud.com>
2021-03-11 17:31:42 +0900

    boardとtagのアソシエーションの設定を行った

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  comment    :text(65535)      not null
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  board_id   :integer
+#
+# Indexes
+#
+#  index_comments_on_board_id  (board_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (board_id => boards.id)
+#
 class Comment < ApplicationRecord
   belongs_to :board # #commentはboardに紐づく（多のCommentの方に1のモデルを紐付ける）
   validates :name, presence: true, length: { maximum: 10 }
--- test/models/board_tag_relation_test.rb
+++ test/models/board_tag_relation_test.rb
@@ -1,25 +1,26 @@
 # == Schema Information
 #
-# Table name: comments
+# Table name: board_tag_relations
 #
 #  id         :integer          not null, primary key
-#  comment    :text(65535)      not null
-#  name       :string(255)      not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  board_id   :integer
+#  tag_id     :integer
 #
 # Indexes
 #
-#  index_comments_on_board_id  (board_id)
+#  index_board_tag_relations_on_board_id  (board_id)
+#  index_board_tag_relations_on_tag_id    (tag_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (board_id => boards.id)
+#  fk_rails_...  (tag_id => tags.id)
 #
 require 'test_helper'
 
-class CommentTest < ActiveSupport::TestCase
+class BoardTagRelationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
