dd9f0ea99254183ee19bd4b2466232af75485033
yumaasato <asson0730@icloud.com>
2021-03-11 14:47:35 +0900

    コメント機能を実装した

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -23,7 +23,7 @@ class BoardsController < ApplicationController
   end
 
   def show
-    @comment = @board.comments.new ##掲示板に紐づいた新しいcommentを作成することができる
+    @comment = Comment.new(board_id: @board.id)
   end
 
   def edit
