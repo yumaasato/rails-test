ce010be3efb3d3d7a98a30aa5b30b91840b1db09
yumaasato <asson0730@icloud.com>
2021-03-12 13:23:04 +0900

    マイページにユーザー名が表示されるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :current_user
+
+  private
+
+  def current_user
+    return unless session[:user_id] ##sessionにuser_idの情報がなければreturnする
+    @current_user = User.find_by(id: session[:user_id])
+  end
 end
