a3948c87c3b6112a46d098e8bfb55633492cd63e
yumaasato <asson0730@icloud.com>
2021-03-12 12:15:51 +0900

    ユーザー登録フォームを作成した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'mypage', to: 'users#me'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions$destroy'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   resources :users, only: %i[new create]
