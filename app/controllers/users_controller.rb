8e652a26cc3b92e361decfe861ca556254b3c0dd
yumaasato <asson0730@icloud.com>
2021-03-12 12:46:49 +0900

    ユーザーを登録できるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    @user = User.new(flash[:user]) ##else以下の処理に入ったとしてもフォームにuser名が消えずに残る
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      redirect_to :back, flash: {
+        user: user, ##userオブジェクト
+        error_messages: user.errors.full_messages
+      }
+    end
   end
 
   def me
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
